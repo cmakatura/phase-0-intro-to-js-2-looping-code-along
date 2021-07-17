f8d3c625550646ea5b76a8187479be094f5724a6
christina makatura <cnmak@comcast.net>
2021-07-16 22:57:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  } 
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const messages = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages; 
+}
+//writeCards(["Ada", "Brendan", "Ali"], "birthday");
+//writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+
+function countDown(int) {
+let i = 10;
+while (i >= 0) {
+    console.log(i);
+        i--;
+}
+return int;
+}
+
